d1d03fbbf018bf73acc0c8349b4983557d6321c7
Kacper Szurek <kacperszurek@gmail.com>
2012-03-15 15:07:14 +0100

    Inicjalizacja czystego dokuwiki

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+kacka:63a9f0ea7bb98050796b649e85481845:Kacper Szurek:kacperszurek@gmail.com:admin,user
