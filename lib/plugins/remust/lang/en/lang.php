8df10e0e0cc7494c77e194ff880296b9e60ca0a6
Kacper Szurek <kacperszurek@gmail.com>
2013-01-03 18:26:45 +0100

    Dodanie tlumaczen

---
+++ lib/plugins/remust/lang/en/lang.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Kacper Szurek <kacperszurek@gmail.com> 
+ */
+
+$lang['remust_name'] = 'ReMust';
+$lang['remust_page_link'] = 'Request reading';
+$lang['remust_page_not_exist'] = 'Page %s does not exist';
+$lang['remust_select_users'] = 'Choose users';
+$lang['remust_save_success'] = 'Data has been successfully save.';
+$lang['remust_before_read_text'] = 'User <b>%s</b> request that you should read that page.';
+$lang['remust_confirm_read'] = 'Confirm';
+$lang['remust_confirmed_read'] = 'Confirm that you read this page';
+$lang['remust_mail_info_subject'] = 'Request reading';
+$lang['remust_mail_info_body'] = "Hello,\nSomeone request that you should read page: %s";
+$lang['remust_page'] = 'Page';
+$lang['remust_ask_date'] = 'Data';
+$lang['remust_asker'] = 'Who request';
+$lang['remust_confirm_date'] = 'Readed at';
+$lang['remust_user'] = 'User';
+$lang['remust_not_your_data'] = 'You cannot delete user %s';
+$lang['remust_change_rev'] = 'Change revision';
+$lang['remust_newest_rev'] = 'The newest revision';
+$lang['remust_rev_not_exist'] = 'This revision does not exist';
+$lang['remust_must_login'] = 'You should log in to view this page.';
+$lang['remust_cannot_read_this_page'] = 'You do not have permission to read this page.';
+$lang['remust_concurrency_error'] = 'Someone was editing this page. Please try again.';
+$lang['js']['remust_asc'] = 'Ascending';
+$lang['js']['remust_desc'] = 'Descending';
+$lang['js']['remust_first'] = 'First';
+$lang['js']['remust_last'] = 'Last';
+$lang['js']['remust_prev'] = 'Previous';
+$lang['js']['remust_next'] = 'Next';
+$lang['js']['remust_nodata'] = 'No data to display';
+$lang['js']['remust_rinfo'] = 'Records  _START_ - _END_ total: _TOTAL_';
+$lang['js']['remust_loading'] = 'Loading';
+$lang['js']['remust_search'] = 'Search';
+$lang['js']['remust_records'] = 'Records per page _MENU_';
+$lang['js']['remust_type'] = 'Type part of name';
+$lang['js']['remust_notfound'] = 'Not found';
+$lang['js']['remust_searching'] = 'Searching';
+
+
